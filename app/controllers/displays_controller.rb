36e2352fec75f3c2763f94adf20bf8df95e5c218
Lavika Makwana <lavika.makwana03@gmail.com>
2021-11-08 11:43:23 +0530

    Quick fix

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,9 +1,9 @@
 class DisplaysController < ApplicationController
   def home_page
-    @content_types = Content.pluck(:types).uniq
+    @content_types = Content.pluck(:content_type).uniq
   end
 
   def display_type_content
-    @type_lists = Content.where(types: params[:id])
+    @type_lists = Content.where(content_type: params[:id])
   end
 end
