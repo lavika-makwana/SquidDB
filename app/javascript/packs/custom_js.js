aaeb728e5aac27a3790377c14c67ecef99120ac1
Lavika Makwana <lavika.makwana03@gmail.com>
2021-11-08 10:58:44 +0530

    WIP: Fixes when user add a new type

--- app/javascript/packs/custom_js.js
+++ app/javascript/packs/custom_js.js
@@ -1,16 +1,18 @@
 $( document ).ready(function() {
-
+  $("input#content_types").attr("disabled", true);
   $(".add-type").hide();
   $("#season-area").hide();
   
-
   $('select#content_types').on('input', function() {
     if(this.value == 'Add a new type')
     {
       $(".add-type").show();
+      $("input#content_types").attr("disabled", false);
     }
     else{
       $(".add-type").hide();
+      $("input#content_types").attr("disabled", true);
+
     }
   });
 
